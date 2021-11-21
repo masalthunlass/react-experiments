97d2eac0ad430725c325eb1fa3ae3d01b0f96539
masalthunlass <masalthunlass@gmail.com>
2021-11-21 14:29:43 +0100

    fix submit button

--- src/component/submit-button/SubmitButton.js
+++ src/component/submit-button/SubmitButton.js
@@ -2,7 +2,7 @@ import React from "react";
 import './SubmitButton.css';
 
 const SubmitButton = ({submit, label}) => {
-    return <><button onSubmit={submit} type="submit" className="submit-button">{label}</button></>;
+    return <><button onClick={submit} type="submit" className="submit-button">{label}</button></>;
 }
 
 export default SubmitButton;
