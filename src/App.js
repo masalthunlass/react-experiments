e3629a2e1be2e13457203005aed8db70d2e91b05
masalthunlass <masalthunlass@gmail.com>
2021-11-21 14:50:42 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import StartGamePanel from "./component/start-game-panel/StartGamePanel";
 import DispatchPlayersPanel from "./component/dispatch-players-panel/DispatchPlayersPanel";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
     return <div>
 
         <h1>Longueur d'ondes</h1>
-        <StartGamePanel/>
-        <DispatchPlayersPanel/>
+        <Routes>
+            <Route path="/" element={ <StartGamePanel/>} />
+            <Route path="startGame" element={<DispatchPlayersPanel/>} />
+        </Routes>
+
+
     </div>;
 }
 
