0f60917360c1a50c274dfafd253618ec2a2a72a9
masalthunlass <masalthunlass@gmail.com>
2021-12-08 20:18:32 +0100

    added a post api to save a game in file

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
+const gameController = require('./api/GameController')
+
 const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: __dirname});
+app.use(express.json());
 
+
+app.post('/api/games', (req, res) => {
+    gameController.createGame(req.body.id, req.body.firstPlayer).then((games) => {
+        res.status(200).json(games);
+    }).catch((err) => {
+        res.status(500).json(err);
+    });
 });
+
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
